31b52715dc441956a9a23d2c61880691da353454
Michael Maxwell <mike.ian.maxwell@gmail.com>
2026-05-22 13:10:03 -0400

    hide latency charts

--- uptime.config.ts
+++ uptime.config.ts
@@ -22,6 +22,7 @@ const workerConfig: WorkerConfig = {
       target: 'https://micmax.pw',
       statusPageLink: 'https://micmax.pw',
       checkProxy: 'worker://enam',
+      hideLatencyChart: true,
       expectedCodes: [200],
       timeout: 10000,
       headers: {
@@ -35,6 +36,7 @@ const workerConfig: WorkerConfig = {
       target: 'https://maxwellmade.ca',
       checkProxy: 'worker://enam',
       statusPageLink: 'https://maxwellmade.ca',
+      hideLatencyChart: true,
       expectedCodes: [200],
       timeout: 10000,
       headers: {
@@ -48,6 +50,7 @@ const workerConfig: WorkerConfig = {
       target: 'https://site.micmax.pw',
       checkProxy: 'worker://enam',
       statusPageLink: 'https://site.micmax.pw',
+      hideLatencyChart: true,
       expectedCodes: [200],
       timeout: 10000,
       headers: {
@@ -61,6 +64,7 @@ const workerConfig: WorkerConfig = {
       target: 'mumble.micmax.pw:64738',
       checkProxy: 'worker://enam',
       statusPageLink: 'mumble://mumble.micmax.pw',
+      hideLatencyChart: true,
       timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare',
@@ -73,6 +77,7 @@ const workerConfig: WorkerConfig = {
       target: 'minecraft.micmax.pw:25565',
       checkProxy: 'worker://enam',
       statusPageLink: 'minecraft.micmax.pw',
+      hideLatencyChart: true,
       timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare',
@@ -85,6 +90,7 @@ const workerConfig: WorkerConfig = {
       target: 'https://jellyfin.micmax.pw',
       checkProxy: 'worker://enam',
       statusPageLink: 'https://jellyfin.micmax.pw',
+      hideLatencyChart: true,
       expectedCodes: [200],
       timeout: 10000,
       headers: {
