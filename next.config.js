e2897c83651fd13d4ce0884b9c217c250a4e51ed
Michael Maxwell <mike.ian.maxwell@gmail.com>
2026-06-16 17:28:01 -0400

    Old kv stuff removed

--- next.config.js
+++ next.config.js
@@ -4,15 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-if (process.env.NODE_ENV === 'development') {
-  const { setupDevBindings } = require('@cloudflare/next-on-pages/next-dev')
-  setupDevBindings({
-    bindings: {
-      UPTIMEFLARE_STATE: {
-        type: 'kv',
-        id: 'UPTIMEFLARE_STATE',
-      },
-    },
-  })
-}
