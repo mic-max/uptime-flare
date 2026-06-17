453cb2226ca642aa8cac45a54c3036c1b3b2a73e
Michael Maxwell <mike.ian.maxwell@gmail.com>
2026-06-16 21:32:56 -0400

    Page size optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Tree-shake large barrel packages so only the icons/components actually used
+  // end up in the bundle (cuts "unused JavaScript").
+  experimental: {
+    optimizePackageImports: ['@tabler/icons-react', '@mantine/core'],
+  },
 }
 
 module.exports = nextConfig
